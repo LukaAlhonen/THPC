3b1aca053f0ba33061abe00ac26d735e35be9614
LukaAlhonen <luka.alhonen@protonmail.com>
2024-02-01 19:39:23 +0200

    Commit

--- luka_alhonen_ex03/p03/ex3p3.c
+++ luka_alhonen_ex03/p03/ex3p3.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #define N 15000
 int a[N][N];
@@ -8,21 +9,26 @@ int a[N][N];
 void main() {
   int i,j;
   clock_t t1,t2;
+  double cpu_time;
 
 
   /* Begin measurement */
+  t1 = clock();
+  
   for (i=0;i<N;i++) 
     for (j=0;j<N;j++) 
       a[i][j]=(i+j)/2;
+  
+  t2 = clock();
+  cpu_time = (double) (t2-t1) / CLOCKS_PER_SEC;
   /* End measurement */
-
 
   for (i=0;i<N;i+=NPRINT)
     for (j=0;j<N;j+=NPRINT)
       printf("%d ",a[i][j]);
   printf("\n");
 
-
+  printf("Time elapsed: %f s\n", cpu_time);
 }
 
 
