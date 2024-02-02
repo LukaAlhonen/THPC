e2043c4e16590a5831b508c64c88346ab438d54b
LukaAlhonen <luka.alhonen@protonmail.com>
2024-02-02 16:05:11 +0200

    Added solutions to ex03

--- luka_alhonen_ex03/p04/p04_cpu.c
+++ luka_alhonen_ex03/p04/p04_cpu.c
@@ -1,22 +1,59 @@
-#include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv)
-{
-    clock_t t1,t2;
-    double cpu_time;
-    int n = 1000000;
+double expensive_operation(int n) {
     double res = 0.0;
-
+    clock_t t1,t2;
+    
     t1 = clock();
+    for (int k = 0; k < n; k++) {
+        res += exp(sin(k/1000000));
+    }
+    t2 = clock();
+
+    return (double)(t2 - t1) / CLOCKS_PER_SEC;
+}
+
+double expensive_operation_print(int n) {
+    double res = 0.0;
+    clock_t t1,t2;
+    
+    t1 = clock();    
     for (int k = 0; k < n; k++) {
         res += exp(sin(k/1000000));
         printf("res: %f\n", res);
     }
     t2 = clock();
-    cpu_time = (double) (t2-t1) / CLOCKS_PER_SEC;
+
+    return (double)(t2 - t1) / CLOCKS_PER_SEC;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Too few arguments, please specify number of iterations\n");
+        return 1;
+    }
+
+    clock_t t1,t2;
+    double cpu_time;
+    double res = 0.0;
+
+    int n = atoi(argv[1]);
+    if (n == 0 && argv[1][0] != '0') {
+        printf("Invalid input\n");
+        return 1;
+    }
+    
+    if (argc == 3 && strcmp(argv[2], "verbose") == 0) {
+        cpu_time = expensive_operation_print(n);
+    } else {
+        cpu_time = expensive_operation(n);
+    }
+
     printf("Elapsed CPU time: %f\n", cpu_time);
 
     return 0;
--- luka_alhonen_ex03/p04/p04_wall.c
+++ luka_alhonen_ex03/p04/p04_wall.c
@@ -1,15 +1,27 @@
-#include <stdlib.h>
 #include <math.h>
 #include <sys/time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv)
-{
+double expensive_operation(int n) {
+    double res = 0.0;
     struct timeval tp1, tp2;
     struct timezone tz1, tz2;
-    int n = 1000000;
-    double res = 0.0;
+    double wall_time;
+    gettimeofday(&tp1, &tz1);
+    for (int k = 0; k < n; k++) {
+        res += exp(sin(k/1000000));
+    }
+    gettimeofday(&tp2, &tz2);
 
+    return (tp2.tv_sec - tp1.tv_sec) + (tp2.tv_usec - tp1.tv_usec) / 1000000.0;
+}
+
+double expensive_operation_print(int n) {
+    double res = 0.0;
+    struct timeval tp1, tp2;
+    struct timezone tz1, tz2;
     gettimeofday(&tp1, &tz1);
     for (int k = 0; k < n; k++) {
         res += exp(sin(k/1000000));
@@ -17,6 +29,30 @@ int main(int argc, char **argv)
     }
     gettimeofday(&tp2, &tz2);
 
-    printf("Time elapsed actual time: %f s\n", (tp2.tv_sec - tp1.tv_sec) + (tp2.tv_usec - tp1.tv_usec) / 1000000.0);
+    return (tp2.tv_sec - tp1.tv_sec) + (tp2.tv_usec - tp1.tv_usec) / 1000000.0;
+}
+
+int main(int argc, char **argv)
+{
+    if(argc < 2) {
+        printf("Too few arguments, please specify number of iterations\n");
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    if (n == 0 && argv[1][0] != '0') {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    double wall_time;
+
+    if (argc == 3 && strcmp(argv[2], "verbose") == 0) {
+        wall_time = expensive_operation_print(n);
+    } else {
+        wall_time = expensive_operation(n);
+    }
+
+    printf("Time elapsed actual time: %f s\n", wall_time);
     return 0;
 }
